540e7d8bb5a7e43b5bad2d7509f4f5968fb692d2
Aleksandr <a.kravchenko.intek@gmail.com>
2020-11-11 01:32:10 +0100

    navbar styled, dropdown menu, font-awesome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_helper
+    if user_signed_in?
+      image_tag "https://avatars3.githubusercontent.com/u/22182318?s=400&u=8262e9bd2906ddcceed183ddc2acb82c1500ca83&v=4"
+    else
+      icon('fas', 'user-circle', style: 'font-size: 35px; color:white;')
+    end
+  end
 end
