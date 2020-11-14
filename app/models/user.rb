07f9a2ca0eba2c6cd0fb7da2cd9cd47cd524fa4e
Aleksandr <a.kravchenko.intek@gmail.com>
2020-11-14 23:20:39 +0100

    emai validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable
-         
+
   enum role: [:client, :driver, :admin]
 end
