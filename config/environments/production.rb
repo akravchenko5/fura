4317034003dc16c62db8214143db4ad7e36fee2a
Aleksandr <a.kravchenko.intek@gmail.com>
2020-11-15 14:02:45 +0100

    SendGrip workaround;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fura_production"
 
-  config.action_mailer.default_url_options = { host: 'fura-me.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { :host => 'fura-me.herokuapp.com', :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
