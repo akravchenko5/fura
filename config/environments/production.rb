52cc434d17164d166346e7aa7afce418d898e002
Aleksandr <a.kravchenko.intek@gmail.com>
2020-11-15 01:20:02 +0100

    test mail function

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "fura_production"
 
-  config.action_mailer.default_url_options = { host: 'fura-me.herokuapp.com', protocol: 'https' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default_url_options = { host: 'fura-me.herokuapp.com', protocol: 'https' }
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
