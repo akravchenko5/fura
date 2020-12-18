a4c98802e6b9d0342be4f71dfecfed39bca90765
Aleksandr <a.kravchenko.intek@gmail.com>
2020-12-18 18:46:19 +0100

    serach form, flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,16 @@
 // that code so it'll be compiled.
 // import '../stylesheets/application.scss';
 import 'bootstrap';
+import { dateTimeFlatpickr } from './components/date_flatpckr';
 
 require('@rails/ujs').start();
 require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels');
+
+document.addEventListener('turbolinks:load', () => {
+  dateTimeFlatpickr();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
